e8220dbaf3924c205aa7ac872d01f1c8fbe1357a
KunZhang99 <c2033216@Newcastle.ac.uk>
2023-04-23 22:23:34 +0100

    send request

--- app/src/main/java/com/example/mycontentpages/Utils/OkHttp.java
+++ app/src/main/java/com/example/mycontentpages/Utils/OkHttp.java
@@ -11,8 +11,8 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class OkHttp {
-    private static final OkHttpClient httpClient = new OkHttpClient();
 
+    private static final OkHttpClient httpClient = new OkHttpClient();
 
     public static String sendGetRequest(String url) throws IOException {
         Request request = new Request.Builder()
@@ -20,12 +20,8 @@ public class OkHttp {
                 .build();
 
         try (Response response = httpClient.newCall(request).execute()) {
-            if (!response.isSuccessful()) throw new IOException("Unexpected code: " + response);
-            String responseBody = response.body().string();
-            System.out.println(responseBody);
-            return "111";
+            return response.body().string();
         }
-
     }
 
     public static String sendPostRequest(String url, String jsonBody) throws Exception{
@@ -37,23 +33,11 @@ public class OkHttp {
                 .post(requestBody)
                 .build();
 
-        Log.e("TAG-ONE", jsonBody + "post");
-
         Response response = httpClient.newCall(request).execute();
         if (response==null){Log.i("response","response is null");}
         String resData = response.body().string();
-        Log.e("TAG-ONE","2222222222 "+resData);
 
         return resData;
-
-//        try {Response response = httpClient.newCall(request).execute();
-//            Log.e("TAG-ONE", response.body().string() + "post");
-//            return response.body().string();
-//        }catch (Exception e){
-//            Log.e("TAG-ONE","2222222222 "+e.getMessage());
-//
-//        }
-//        return "null";
     }
 
 }
