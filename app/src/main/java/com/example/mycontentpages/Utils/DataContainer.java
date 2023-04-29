34a5fd37fe6aa8cd7aa5741a05f0d1848e75b0c5
J.Yu31@newcastle.ac.uk <J.Yu31@newcastle.ac.uk>
2023-04-29 21:11:40 +0100

    DataContainer中的请求方法迁移到okhttp类中

--- app/src/main/java/com/example/mycontentpages/Utils/DataContainer.java
+++ app/src/main/java/com/example/mycontentpages/Utils/DataContainer.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.alibaba.fastjson.JSON;
 import com.example.mycontentpages.data.Place;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
 import okhttp3.OkHttpClient;
@@ -15,47 +16,22 @@ import okhttp3.ResponseBody;
 @Data
 public class DataContainer {
 
+
     // TODO: 28/04/2023  需要初始化空arraylist，防止为空是没有初始值，程序终止
-    private static List<Place> placeContainer;
+    private static List<Place> placeContainer=new ArrayList<>();
 
    private static final OkHttpClient okHttpClient=new OkHttpClient();
         public static void initialize() throws IOException {
             Log.i("DATA","data initializing");
-            initPlaces();
-
+            OkHttp.initPlaces();
         }
 
-        public static void initPlaces() throws IOException {
-//            String s = OkHttp.sendGetRequest("http://172.20.10.3:8090/place/get");
-//            Log.e("body",s);
-//            List<Place> places = JSON.parseArray(s, Place.class);
-//            placeContainer=places;
 
-
-            Request request = new Request.Builder()
-                   .url("http://172.20.10.3:8090/place/get")
-                    //  .url("http://10.183.135.5:8090/place/get")
-                    .build();
-            Response response = okHttpClient.newCall(request).execute();
-
-            if (response.isSuccessful()==false)
-            { Request request2 = new Request.Builder()
-                   //    .url("http://172.20.10.3:8090/place/get")
-                    .url("http://10.183.135.5:8090/place/get")
-                    .build();
-             response=okHttpClient.newCall(request2).execute();
-            }
-                ResponseBody body = response.body();
-                String jsonStr = body.string();
-                Log.i("body", jsonStr);
-                List<Place> places = JSON.parseArray(jsonStr, Place.class);
-                placeContainer = places;
-
-
-
-        }
         public static  List<Place> getPlaceContainer(){
             return placeContainer;
+        }
+        public static void setPlaceContainer( List<Place> places){
+            placeContainer=places;
         }
 
 }
--- app/src/main/java/com/example/mycontentpages/Utils/OkHttp.java
+++ app/src/main/java/com/example/mycontentpages/Utils/OkHttp.java
@@ -1,8 +1,13 @@
 package com.example.mycontentpages.Utils;
 
+import android.provider.ContactsContract;
 import android.util.Log;
 
+import com.alibaba.fastjson.JSON;
+import com.example.mycontentpages.data.Place;
+
 import java.io.IOException;
+import java.util.List;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -11,9 +16,7 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class OkHttp {
-
-    public static final String Base_url = "http://10.58.212.5:8090";
-
+    public static final String Base_url = "http://172.20.10.3:8090";
     public static String sendGetRequest(String url) throws IOException {
         OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(new HandlerUtils())
@@ -27,6 +30,34 @@ public class OkHttp {
             return response.body().string();
         }
     }
+
+    public static void initPlaces() throws IOException {
+        String s = OkHttp.sendGetRequest("/place/get");
+        Log.e("body",s);
+        List<Place> places = JSON.parseArray(s, Place.class);
+       DataContainer.setPlaceContainer(places);
+
+//            Request request = new Request.Builder()
+//                   .url("http://172.20.10.30:8090/place/get")
+//                    //  .url("http://10.183.135.5:8090/place/get")
+//                    .build();
+//            Response response = okHttpClient.newCall(request).execute();
+//                Log.e("HTTP", "" + response.isSuccessful());
+//            if (response.isSuccessful()==false)
+//            {
+//                Request request2 = new Request.Builder()
+//                   //    .url("http://172.20.10.3:8090/place/get")
+//                    .url("http://10.183.135.5:8090/place/get")
+//                    .build();
+//             response=okHttpClient.newCall(request2).execute();
+//            }
+//                ResponseBody body = response.body();
+//                String jsonStr = body.string();
+//                Log.i("body", jsonStr);
+//                List<Place> places = JSON.parseArray(jsonStr, Place.class);
+//                placeContainer = places;
+    }
+
 
     public static String sendPostRequest(String url, String jsonBody) throws Exception{
         MediaType JSON = MediaType.parse("application/json; charset=utf-8");
